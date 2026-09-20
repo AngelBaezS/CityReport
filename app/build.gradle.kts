da0a43b5060df63cd9695d0a02b827b0b8574555
Angel Baez <angelbaezs1512@gmail.com>
2026-09-20 17:54:31 -0600

    Feat: Change AGP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.upiicsa.cityreport"
-    compileSdk {
-        version = release(37)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.upiicsa.cityreport"
         minSdk = 35
-        targetSdk = 37
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -21,9 +19,11 @@ android {
 
     buildTypes {
         release {
-            optimization {
-                enable = false
-            }
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
